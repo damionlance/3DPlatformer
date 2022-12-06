2f6526f484f87326adbde642a706fd79bc0831dc
davidhartman <djhartman1216@gmail.com>
2022-12-06 09:36:37 -0500

    Side flip first iteration is added

--- Scripts/StateMachine/States/SideFlip.gd
+++ Scripts/StateMachine/States/SideFlip.gd
@@ -3,16 +3,35 @@ extends AerialMovement
 class_name SideFlip
 
 var _state_name = "SideFlip"
+
 func _ready():
 	_state.state_dictionary[_state_name] = self
 	_state.update_state(_state_name)
 
-func update():
+func update(delta):
+	_player.anim_tree.travel("Side Flip")
+	
+	if wall_jump_collision_check() and _state._allow_wall_jump:
+		_state.update_state("WallSlide")
+		return
+	if _player.is_on_floor():
+		if _state.input_direction:
+			_state.update_state("Running")
+			return
+		_state.snap_vector = Vector3.DOWN
+		_state.update_state("Idle")
+		_state.just_landed = true
+		return
+	
+	_state.velocity = _state.calculate_velocity(_fall2_gravity, delta)
 	
 	pass
 
 func reset():
-	
+	_state.move_direction = _state.move_direction
+	_state.velocity.y = _jump2_strength * 2
+	_state.snap_vector = Vector3.ZERO
+	_player.transform = _player.transform.looking_at(_player.global_transform.origin + _state.move_direction, Vector3.UP)
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
