9c44aec49cf284f6def905c5a3a23d7c570451e7
David Hartman <djhartman1216@gmail.com>
2023-03-07 12:17:14 -0500

    Initial save system implemented

--- MiscScenes/Collectables/Coin/Coin.gd
+++ MiscScenes/Collectables/Coin/Coin.gd
@@ -1,16 +1,18 @@
-extends Node
+extends Collectable
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
+	
 	pass
 
 
 func _on_coin_body_entered(body):
 	if body.get_name() == "Player":
+		collected = true
+		Global.UPDATE_COLLECTIBLES(name, collected)
 		if body.add_coin():
 			queue_free()
