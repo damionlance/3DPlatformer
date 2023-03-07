8adef60304c453585ebca22f48fd374f3253eeb8
David Hartman <djhartman1216@gmail.com>
2023-03-07 12:20:39 -0500

    I fix damion bistake

--- MiscScenes/Collectables/Coin/Coin.gd
+++ MiscScenes/Collectables/Coin/Coin.gd
@@ -1,10 +1,5 @@
 extends Collectable
 
-
-# Called when the node enters the scene tree for the first time.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-
 func _process(delta):
 	
 	pass
