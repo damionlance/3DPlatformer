f7e09db84d4468249919834f62ac3399577a725f
damionlance <damion.lance@gmail.com>
2023-03-08 09:16:08 -0500

    Updated collectable behavior after being collected

--- MiscScenes/Collectables/Coin/Coin.gd
+++ MiscScenes/Collectables/Coin/Coin.gd
@@ -1,13 +1,23 @@
 extends Collectable
 
-func _process(delta):
-	
-	pass
+const materials : Array = [
+	preload("res://MiscScenes/Collectables/Coin/coin.tres"),
+	preload("res://MiscScenes/Collectables/Coin/coinCollected.tres")
+]
 
+@onready var coinMesh = $Coin/Cylinder
+
+func _process(delta):
+	if collected:
+		change_material(1)
+	else:
+		change_material(0)
+
+func change_material(newMaterialIndex):
+	coinMesh.set_surface_override_material(0, materials[newMaterialIndex])
 
 func _on_coin_body_entered(body):
 	if body.get_name() == "Player":
 		collected = true
 		Global.UPDATE_COLLECTIBLES(name, collected)
-		if body.add_coin():
-			queue_free()
+		body.add_coin()
--- MiscScenes/Collectables/PoolCoin/PoolCoin.gd
+++ MiscScenes/Collectables/PoolCoin/PoolCoin.gd
@@ -1,17 +1,25 @@
 extends Collectable
 
 
-# Called when the node enters the scene tree for the first time.
+const materials : Array = [
+	preload("res://MiscScenes/Collectables/PoolCoin/PoolCoin.tres"),
+	preload("res://MiscScenes/Collectables/PoolCoin/PoolCoinCollected.tres")
+]
 
+@onready var coinMesh = $Coin/Cylinder
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if collected:
+		change_material(1)
+	else:
+		change_material(0)
 
+func change_material(newMaterialIndex):
+	coinMesh.set_surface_override_material(0, materials[newMaterialIndex])
 
 func _on_body_entered(body):
 	if body.get_name() == "Player":
 		collected = true
 		Global.UPDATE_COLLECTIBLES(name, collected)
-		if body.add_levelcoin(0):
-			queue_free()
+		body.add_levelcoin(0)
