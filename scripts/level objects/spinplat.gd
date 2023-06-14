1b07bde4f58fa2cecd752ac36751ee99a5c4a59d
David Hartman <djhartman1216@gmail.com>
2023-06-14 11:05:29 -0400

    Fixed spinning plats

--- scripts/level objects/spinplat.gd
+++ scripts/level objects/spinplat.gd
@@ -1,17 +1,20 @@
 @tool
-extends MeshInstance3D
+extends Node3D
 
 var tween : Tween
 var localHeight
 @export var spinHeight : Vector3
 @export var risingSpeed : float
 @export var fallingSpeed : float
+@export var mesh : MeshInstance3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#localHeight = global_position - spinHeight
-	localHeight = global_position
-	#global_position = localHeight
+	if Engine.is_editor_hint():
+		global_position = mesh.global_position
+	else:
+		mesh.global_position = global_position
+		localHeight = global_position
 
 func _update_localHeight(valueToUpdate):
 	localHeight += valueToUpdate
@@ -19,13 +22,15 @@ func _update_localHeight(valueToUpdate):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Engine.is_editor_hint():
-		#global_position = localHeight + spinHeight
-		pass
+		mesh.global_position = global_position + spinHeight
 
 
 func _on_spin_button_spinning(isSpin):
 	tween = create_tween()
+	var distance
 	if isSpin:
-		tween.tween_property(self, "global_position", localHeight + spinHeight, (localHeight + spinHeight).length()/risingSpeed)
+		distance = (mesh.global_position - (global_position + spinHeight)).length()
+		tween.tween_property(mesh, "global_position", global_position + spinHeight, distance/risingSpeed)
 	else:
-		tween.tween_property(self, "global_position", localHeight, (localHeight + spinHeight).length()/fallingSpeed)
+		distance = (mesh.global_position - global_position).length()
+		tween.tween_property(mesh, "global_position", global_position, distance/fallingSpeed)
