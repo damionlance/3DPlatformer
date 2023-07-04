a096f43f74918c7486bfca9b002c3e9582ee016a
David Hartman <djhartman1216@gmail.com>
2023-07-04 16:16:51 -0400

    Level coins are collectable

--- scripts/level objects/level start/temple.gd
+++ scripts/level objects/level start/temple.gd
@@ -5,4 +5,4 @@ func _ready():
 	$Player/StateMachine.level_loaded = true
 	var children
 	ensure_collectable_exists("COIN")
-	ensure_collectable_exists("POOL COIN")
+	ensure_collectable_exists("LEVEL COIN")
