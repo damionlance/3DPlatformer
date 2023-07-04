f50389d3203d9641deccbb4affacd6ad4beb47dd
David Hartman <djhartman1216@gmail.com>
2023-07-04 16:23:47 -0400

    fixed vines being possible to walk on

--- scripts/tools/climbable_zone.gd
+++ scripts/tools/climbable_zone.gd
@@ -6,4 +6,5 @@ func _ready():
 	for mesh in meshes:
 		get_node(mesh).create_convex_collision()
 		get_node(mesh).add_to_group("climbable zone")
+		get_node(mesh).get_child(0).set_collision_layer(64)
 	
