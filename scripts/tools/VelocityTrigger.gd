a3cb922aadf282a9e4d36e213151f5b2a51a3a3a
David Hartman <djhartman1216@gmail.com>
2023-07-12 21:13:10 -0400

    fixed stomp buttons

--- scripts/tools/VelocityTrigger.gd
+++ scripts/tools/VelocityTrigger.gd
@@ -1,4 +1,4 @@
-extends Area3D
+extends ShapeCast3D
 
 @export var requiredSpeed := 0.0
 
@@ -6,21 +6,22 @@ signal velocity_trigger_fired(body)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if $CollisionShape3D.shape == null:
-		var mdt = MeshDataTool.new()
-		mdt.create_from_surface(get_parent().mesh, 0)
-		var verts = []
-		for i in mdt.get_vertex_count():
-			verts.append(mdt.get_vertex(i))
-		var collisionshape = ConvexPolygonShape3D.new()
-		collisionshape.points = PackedVector3Array(verts)
-		$CollisionShape3D.shape = collisionshape
+	var mdt = MeshDataTool.new()
+	mdt.create_from_surface(get_parent().mesh, 0)
+	var verts = []
+	for i in mdt.get_vertex_count():
+		verts.append(mdt.get_vertex(i))
+	var collisionshape = ConvexPolygonShape3D.new()
+	collisionshape.points = PackedVector3Array(verts)
+	shape = collisionshape
 
-func _on_body_entered(body):
-	print(body.velocity.y)
-	if body is CharacterBody3D:
-		if body.velocity.y < requiredSpeed:
-			emit_signal("velocity_trigger_fired",body)
-	if body is RigidBody3D:
-		if body.linear_velocity.y < requiredSpeed:
-			emit_signal("velocity_trigger_fired", body)
+func _process(delta):
+	for i in get_collision_count():
+		var body = get_collider(i)
+		if body is CharacterBody3D:
+			print(body.velocity.y)
+			if body.velocity.y < requiredSpeed:
+				emit_signal("velocity_trigger_fired",body)
+		if body is RigidBody3D:
+			if body.linear_velocity.y < requiredSpeed:
+				emit_signal("velocity_trigger_fired", body)
