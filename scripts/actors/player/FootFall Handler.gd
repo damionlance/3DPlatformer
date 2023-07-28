f0660966272a441d22b98d6efc87e5ff43f9927e
David Hartman <djhartman1216@gmail.com>
2023-07-28 14:27:29 -0400

    Animation stuff

--- scripts/actors/player/FootFall Handler.gd
+++ scripts/actors/player/FootFall Handler.gd
@@ -16,6 +16,6 @@ func fire_footstep():
 	for body in $"Footstep Particles".get_overlapping_bodies():
 		if body.get_parent() is MeshInstance3D:
 			var floor_material = body.get_parent().mesh["surface_0/material"]
-			#new_particle.draw_pass_1.material = floor_material.duplicate()
-			#new_particle.draw_pass_1.material["shading_mode"] = 0
+			new_particle.draw_pass_1.material = floor_material.duplicate()
+			#new_particle.draw_pass_1.material["shading_mode"] = SHA
 			break
