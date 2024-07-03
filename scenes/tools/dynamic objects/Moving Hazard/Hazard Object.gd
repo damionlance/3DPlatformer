9c169072811b876dfe24728a2eb7ec33e78d1adc
David Hartman <djhartman1216@gmail.com>
2024-07-03 16:07:18 -0400

    Fixed nav meshes

--- scenes/tools/dynamic objects/Moving Hazard/Hazard Object.gd
+++ scenes/tools/dynamic objects/Moving Hazard/Hazard Object.gd
@@ -12,7 +12,8 @@ func _ready() -> void:
 	if get_parent().get_parent().get_parent().has_method("hazard_collided"):
 		connect("collided_with_body", get_parent().get_parent().get_parent().hazard_collided)
 		get_parent().get_parent().get_parent().starts_in_object
-	launch_direction = get_parent().get_parent().launch_direction
+	if "launch_direction" in get_parent().get_parent().get_parent():
+		launch_direction = get_parent().get_parent().get_parent().launch_direction
 
 func _on_body_exited(body: Node3D) -> void:
 	collide = true
